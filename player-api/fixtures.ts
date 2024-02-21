375eea6677466b1542a2206a1f7642df3d6c462f
Nargiz Mamytova <mam.zigran@gmail.com>
2024-02-21 18:22:57 +0600

    Фикстурные значения

--- player-api/fixtures.ts
+++ player-api/fixtures.ts
@@ -2,11 +2,14 @@ import mongoose from "mongoose";
 import config from "./config";
 import User from "./models/User";
 import Artist from "./models/Artist";
+import Album from "./models/Album";
+import Track from "./models/Track";
 
 const dropCollection = async (db: mongoose.Connection, collectionName: string) => {
-    try{
-       await db.dropCollection(collectionName);
-    }catch (e) {
+    try {
+        await db.dropCollection(collectionName);
+    }
+    catch (e) {
         console.log(`Collection ${collectionName} was missing. skipping derop ...`)
     }
 };
@@ -14,7 +17,7 @@ const run = async () => {
     await mongoose.connect(config.mongoose.db);
     const db = mongoose.connection;
     const collections = ['artists', 'albums', 'tracks', 'trackHistory', 'users'];
-    for (const collectionName of collections){
+    for (const collectionName of collections) {
         await dropCollection(db, collectionName)
     }
     const [user1, user2, user3] = await User.create({
@@ -34,9 +37,9 @@ const run = async () => {
         });
     const [artist1, artist2, artist3] = await Artist.create(
         {
-        name: 'MiaGy',
-        image: 'fixtures/miyagi.jpg',
-        id: crypto.randomUUID()
+            name: 'MiaGy',
+            image: 'fixtures/miyagi.jpg',
+            id: crypto.randomUUID()
         },
         {
             name: 'Eminem',
@@ -49,7 +52,157 @@ const run = async () => {
             id: crypto.randomUUID()
         },
     )
+    const [album1, album2, album3, album4, album5, album6, album7] = await Album.create(
+        {
+            id: crypto.randomUUID(),
+            title: 'Yamakasi',
+            artist: artist1.id,
+            issueDate: 2020,
+            image: 'fixtures/yamakasi.jpeg'
+        },
+        {
+            id: crypto.randomUUID(),
+            title: 'Hattori',
+            artist: artist1.id,
+            issueDate: 2022,
+            image: 'fixtures/hattori.jpeg'
+        },
+        {
+            id: crypto.randomUUID(),
+            title: 'Buster Ceaton',
+            artist: artist1.id,
+            issueDate: 2020,
+            image: 'fixtures/bustr.jpeg'
+        },
+        {
+            id: crypto.randomUUID(),
+            title: 'Zima',
+            artist: artist3.id,
+            issueDate: 2022,
+            image: 'fixtures/zima.jpeg'
+        },
+        {
+            id: crypto.randomUUID(),
+            title: 'Сияй',
+            artist: artist3.id,
+            issueDate: 2018,
+            image: 'fixtures/siyay.jpeg'
+        },
+        {
+            id: crypto.randomUUID(),
+            title: 'Relaps: Refill',
+            artist: artist2.id,
+            issueDate: 2009,
+            image: 'fixtures/relaps.jpeg'
+        },
+        {
+            id: crypto.randomUUID(),
+            title: 'The Marshall Mathers LP 2',
+            artist: artist2.id,
+            issueDate: 2009,
+            image: 'fixtures/marshall.jpeg'
+        },
+    )
+    const [track1, track2, track3, track4, track5, track6, track7, track8] = await Track.create(
+        {
+            id: crypto.randomUUID(),
+            title: 'Еще хочу',
+            album: album5.id,
+            duration: '3:26',
+            serialNumber: 1
+        },
+        {
+            id: crypto.randomUUID(),
+            title: 'Сияй',
+            album: album5.id,
+            duration: '3:26',
+            serialNumber: 1
+        },
+        {
+            id: crypto.randomUUID(),
+            title: 'Айсберг',
+            album: album4.id,
+            duration: '3:03',
+            serialNumber: 4
+        },
+        {
+            id: crypto.randomUUID(),
+            title: 'Дискотека на двоих',
+            album: album4.id,
+            duration: '3:04',
+            serialNumber: 8
+        },
+        {
+            id: crypto.randomUUID(),
+            title: 'Dr. West Skit',
+            album: album6.id,
+            duration: '1:03',
+            serialNumber: 1
+        },
+        {
+            id: crypto.randomUUID(),
+            title: 'My Mom',
+            album: album6.id,
+            duration: '5:03',
+            serialNumber: 3
+        },
+        {
+            id: crypto.randomUUID(),
+            title: 'Bad Guy',
+            album: album7.id,
+            duration: '7:14',
+            serialNumber: 2
+        },
+        {
+            id: crypto.randomUUID(),
+            title: 'So much better',
+            album: album7.id,
+            duration: '7:14',
+            serialNumber: 1
+        },
+        {
+            id: crypto.randomUUID(),
+            title: 'Intro',
+            album: album3.id,
+            duration: '1:03',
+            serialNumber: 2
+        },
+        {
+            id: crypto.randomUUID(),
+            title: 'Saloon',
+            album: album2.id,
+            duration: '3:03',
+            serialNumber: 1
+        },
+        {
+            id: crypto.randomUUID(),
+            title: 'Временно',
+            album: album2.id,
+            duration: '4:03',
+            serialNumber: 3
+        },
+        {
+            id: crypto.randomUUID(),
+            title: 'Атлант',
+            album: album1.id,
+            duration: '3:07',
+            serialNumber: 1
+        },
+        {
+            id: crypto.randomUUID(),
+            title: 'Психопатия',
+            album: album1.id,
+            duration: '3:29',
+            serialNumber: 2
+        },
+
+
+
+    )
+
+
     await db.close();
 };
 
 void run();
+
