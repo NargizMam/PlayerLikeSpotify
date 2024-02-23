868dcfe37b301d548b3ff1d4dcb92c5e2e2a7781
Nargiz Mamytova <mam.zigran@gmail.com>
2024-02-23 18:08:53 +0600

    Удаление зависимостей к удленным файлам

--- player-front/src/app/store.ts
+++ player-front/src/app/store.ts
@@ -1,12 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
-import {usersReducer} from "../features/users/usersSlice.ts";
 import {artistsReducer} from "../features/artists/artistsSlice.ts";
 import {albumsReducer} from "../features/ albums/albumsSlice.ts";
 import {tracksReducer} from "../features/tracks/tracksSlice.ts";
 
 export const store = configureStore({
   reducer: {
-    users: usersReducer,
     artists: artistsReducer,
     albums: albumsReducer,
     tracks: tracksReducer
